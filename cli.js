b41ca49141ecaa2f4439ea4e3e0b7efebd63cdc4
Kate Bryan <kcbryan@email.unc.edu>
2023-04-23 23:43:19 -0400

    tweak files to support error messages and time zone inputs

--- cli.js
+++ cli.js
@@ -72,7 +72,7 @@ for (let i = 0; i < args.length; i++) {
 
     }
     if(args[i]=='-z') {
-        timezone = args[i];
+        timezone = args[i + 1];
     }
     if(args[i]=='-j') {
         output=true
@@ -92,6 +92,11 @@ function getHelp() {
         console.log("\t-j            Echo pretty JSON from open-meteo API and exit.")
         process.exit(0)   
 }
+
+//begin data collection and output process
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&timezone='+ timezone);
+// Get the data from the request
+const data = await response.json();
 
 function areWeGaloshing(data, days) {
     if(data.daily.precipitation_hours[days]>0) {
@@ -114,15 +119,15 @@ function dayString(days) {
     }
 }
   
-//begin data collection and output process
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&timezone='+ timezone);
-// Get the data from the request
-const data = await response.json();
-
 if(output==true) {
     //process.stdout.write(data.stringify());
     //this may get angry because it isnt using stdout, but we shall seeee
     console.log(data);
+    process.exit(0);
 }
 
 areWeGaloshing(data, days);
+
+
+
+
