94fde83e12b30db4617cbedb85e3c3a21c6590ac
Kate Bryan <kcbryan@email.unc.edu>
2023-04-23 20:19:19 -0400

    add possible solution for help text with arg index 0 access

--- cli.js
+++ cli.js
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
 
+const min = import('minimist');
+const args = process.argv.slice(2)
+
+//console.log(args)
+
+if(args[0]=='-h') {
+    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
+    console.log("\t-h            Show this help message and exit.")    
+    console.log("\t-n, -s        Latitude: N positive; S negative.")    
+    console.log("\t-e, -w        Longitude: E positive; W negative.")    
+    console.log("\t-z            Time zone: uses tz.guess() from moment-timezone by default.")    
+    console.log("\t-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.")    
+    console.log("\t-j            Echo pretty JSON from open-meteo API and exit.")    
+}
+
+// if(h==true) {
+//     console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
+//     console.log("\t-h            Show this help message and exit.")    
+//     console.log("\t-n, -s        Latitude: N positive; S negative.")    
+//     console.log("\t-e, -w        Longitude: E positive; W negative.")    
+//     console.log("\t-z            Time zone: uses tz.guess() from moment-timezone by default.")    
+//     console.log("\t-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.")    
+//     console.log("\t-j            Echo pretty JSON from open-meteo API and exit.")    
+// }
